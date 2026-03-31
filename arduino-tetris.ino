7da9c07bc3e3cf1b96e39239d606d52722646632
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-03-31 02:32:46 -0400

    Reduce timer to 2 minutes

--- arduino-tetris.ino
+++ arduino-tetris.ino
@@ -141,7 +141,7 @@ uint8_t lockResetCount = 0;
 // --- timer ---
 uint32_t remainingTimeMs = 0;
 unsigned long lastTimerUpdate = 0;
-constexpr uint32_t GAME_DURATION_MS = 180000;
+constexpr uint32_t GAME_DURATION_MS = 120000;
 
 uint32_t highScore = 0;
 uint32_t previousHighScore = 0;
@@ -857,6 +857,7 @@ void renderFrame(unsigned long now) {
 void setup() {
   Serial.begin(9600);
 
+  // uint32 ensures 32 bits are written
   // uint32_t zeroScore = 0;
   // EEPROM.put(0, zeroScore);
 
